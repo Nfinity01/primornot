8560c5c2c481168349d13b0c7bfe1dd2aaf6efa3
Nfinity01 <87165735+Nfinity01@users.noreply.github.com>
2022-02-26 22:08:46 +0530

    Add files via upload

---
+++ Primeornot.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primeornot;
+
+/**
+ *
+ * @author Chezhian
+ */
+public class Primeornot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num = 3;
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+    }
+    
